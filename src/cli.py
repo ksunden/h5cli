ae3b22565f91a17bb2fb13f2bddd0fc013241e12
ksunden <sunden@wisc.edu>
2017-07-16 12:48:12 -0500

    Closes #1 Adds rudimentary support for cd, ls, and pwd

--- src/cli.py
+++ src/cli.py
@@ -1,4 +1,36 @@
 from cmd2 import Cmd, options, make_option
+import h5_wrapper
+import sys
 
 class CmdApp(Cmd):
-    pass
+
+    def do_ls(self, args, opts=None):
+        for g in self.explorer.list_groups():
+            print(g+"/")
+        
+        for ds in self.explorer.list_datasets():
+            print(ds)
+
+    def do_cd(self, args, opts=None):
+        if len(args) == 0:
+            args = '/'
+        self.explorer.change_dir(args)
+
+    def do_load(self, args, opts=None):
+        print("Loading "+ args)
+        self.explorer = h5_wrapper.H5Explorer(args)
+
+    def do_pwd(self, args, opts=None):
+        print(self.explorer.working_dir)
+
+    def precmd(self, line):
+        if not line.startswith('load') and (line.endswith('.h5') or line.endswith('.hdf5')):
+            line = 'load ' + line
+        return line
+
+    def do_exit(self, args):
+        return True
+
+if __name__ == '__main__':
+    c = CmdApp()
+    c.cmdloop()
