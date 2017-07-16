03f77cce23e1ea6c4d00ecb17c1187e90a6a7d2f
Lucas Kushner <lphk92@gmail.com>
2017-07-16 11:23:25 -0500

    Add create_group (Closes #12)

--- src/h5_wrapper.py
+++ src/h5_wrapper.py
@@ -8,13 +8,18 @@ class H5Explorer(object):
 
         self.__dir_queue = list()
 
+    def __get_absolute_path(self, path):
+        if not path.startswith("/"):
+            return "/".join(self.__working_dir.split("/") + path.split("/"))
+        else:
+            return path
+
     @property
     def working_dir(self):
         return self.__working_dir
 
     def change_dir(self, new_dir="/"):
-        if not new_dir.startswith("/"):
-            new_dir = "{}/{}".format(self.__working_dir, new_dir)
+        new_dir = self.__get_absolute_path(new_dir)
 
         if new_dir in self.__file:
             self.__working_dir = new_dir
@@ -37,3 +42,7 @@ class H5Explorer(object):
     def list_datasets(self):
         return [ k for k, v in self.__file[self.__working_dir].items()
                  if isinstance(v, h5py.Dataset) ]
+
+    def create_group(self, group_dir):
+        group_dir = self.__get_absolute_path(group_dir)
+        self.__file.create_group(group_dir)
