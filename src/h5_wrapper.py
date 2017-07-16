d4b8aac41b488aa92cc571827ce1165e908b5133
Lucas Kushner <lphk92@gmail.com>
2017-07-16 10:55:54 -0500

    Adding pop_dir and push_dir. Closes #3

--- src/h5_wrapper.py
+++ src/h5_wrapper.py
@@ -6,6 +6,8 @@ class H5Explorer(object):
         self.__file = h5py.File(filename, mode)
         self.__working_dir = "/"
 
+        self.__dir_queue = list()
+
     @property
     def working_dir(self):
         return self.__working_dir
@@ -18,6 +20,15 @@ class H5Explorer(object):
             self.__working_dir = new_dir
         else:
             raise ValueError("Directory {} does not exist.".format(new_dir))
+
+    def push_dir(self, new_dir):
+        self.__dir_queue.append(self.__working_dir)
+        self.change_dir(new_dir)
+
+    def pop_dir(self):
+        if len(self.__dir_queue) == 0:
+            raise IndexError("Cannot pop a directory before pushing one.")
+        self.__working_dir = self.__dir_queue.pop()
 
     def list_groups(self):
         return [ k for k, v in self.__file[self.__working_dir].items()
